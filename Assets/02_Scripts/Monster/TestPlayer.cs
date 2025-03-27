34d7de19863523b52578af108902bf46cc02cf21
kim minjun <k980128@naver.com>
2025-03-27 16:33:32 +0900

    [Feat] Golem sprites with TestScene made.

--- Assets/02_Scripts/Monster/TestPlayer.cs
+++ Assets/02_Scripts/Monster/TestPlayer.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class TestPlayer : MonoBehaviour
 {
     public float moveSpeed = 5f; // 이동 속도
-    private Rigidbody rb; // Rigidbody 컴포넌트
+    private Rigidbody2D rb; // Rigidbody 컴포넌트
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>(); // Rigidbody 초기화
+        rb = GetComponent<Rigidbody2D>(); // Rigidbody 초기화
     }
 
     private void Update()
     {
         // 사용자 입력 처리
         float moveX = Input.GetAxis("Horizontal"); // 좌우 입력
-        float moveZ = Input.GetAxis("Vertical");   // 앞뒤 입력
+        float moveY = Input.GetAxis("Vertical");   // 앞뒤 입력
 
         // 움직임 벡터 계산
-        Vector3 movement = new Vector3(moveX, 0f, moveZ) * moveSpeed;
+        Vector2 movement = new Vector2(moveX, moveY) * moveSpeed;
+
 
         // Rigidbody를 사용한 이동
         rb.velocity = movement;
