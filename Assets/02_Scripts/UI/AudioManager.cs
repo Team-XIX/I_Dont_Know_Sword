6487a9a0442aee4be782636b3848fd6bff26ac5d
Punksoda <zug3049@gmail.com>
2025-03-28 20:07:55 +0900

    [Fix] AudioMixer fuction fix
    I fixed audiomix

---
+++ Assets/02_Scripts/UI/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource BGMaudioSource;
+    public AudioSource SFXaudioSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        if(clip != null && BGMaudioSource != null)
+        {
+            BGMaudioSource.PlayOneShot(clip);
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip != null && SFXaudioSource != null)
+        {
+            SFXaudioSource.PlayOneShot(clip);
+        }
+    }
+}
