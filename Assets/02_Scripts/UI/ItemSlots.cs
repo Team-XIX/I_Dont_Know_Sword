521b33a1d54033c389690fec6739ffe59bb4204d
Punksoda <zug3049@gmail.com>
2025-03-31 12:53:30 +0900

    [Fix] UI fix and add some Scripts
    Fixed UI and add itemSlots and inventory file

---
+++ Assets/02_Scripts/UI/ItemSlots.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor.UI;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class ItemSlot : MonoBehaviour
+{
+   // public ItemData item; 아이템 데이터
+
+ 
+    public Image icon;
+    public TextMeshProUGUI AmmoText;
+    private Outline outline;
+
+    public GameObject equippedIndicator; // 장착된 아이템 표시
+    public event Action<int> onItemSelected; // 슬롯 선택 이벤트
+
+    public int index;
+    public bool equipped;
+  
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    private void OnEnable()
+    {
+        outline.enabled = equipped;
+    }
+
+    public void SetItem()
+    {
+        icon.gameObject.SetActive(true);
+        // icon.sprite = item.icon;
+
+        if (outline != null)
+        {
+            outline.enabled = equipped;
+        }
+    }
+
+    public void Clear()
+    {
+        // item = null;
+        icon.gameObject.SetActive(false);
+        AmmoText.text = string.Empty;
+    }
+
+    public void OnClick()
+    {
+        onItemSelected?.Invoke(index); // 해당 슬롯 선택
+    }
+
+    public void SetEquipped(bool equipped)
+    {
+        equippedIndicator.SetActive(equipped);
+    }
+}
+
