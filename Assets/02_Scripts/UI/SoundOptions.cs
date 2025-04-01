31037173425d44c32abdcf129f45a9d822f5e0f3
Punksoda <zug3049@gmail.com>
2025-04-01 13:44:25 +0900

    [Fix] Change Scene BGM set

--- Assets/02_Scripts/UI/SoundOptions.cs
+++ Assets/02_Scripts/UI/SoundOptions.cs
@@ -1,28 +1,39 @@
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SoundOptions : MonoBehaviour
 {
     public AudioMixer audioMixer;
-
     public Slider Bgmslider;
     public Slider Sfxslider;
 
-    public void SetBgmVoulme()
+    private void Start()
     {
-        audioMixer.SetFloat("BGM", Mathf.Log10((float)Bgmslider.value) * 20);
-        // 로그 함수 Log10은 입력값이 1일때, 0이고 0.1일때 -1, 0.01일때 -2 처럼 감소함
-        // 따라서 슬라이더 값이 작아질수록 출력값이 음수가됨
-        // 로그 값에 20을 곱하는 이유는 데시벨 변환 공식 때문
-        // dB = 20 X 로그10
-        //Bgmslider.value = 1  -> Mathf.Log10(1)  * 20 =  0 dB  (원래 볼륨)
-        //Bgmslider.value = 0.5->Mathf.Log10(0.5) * 20 ≈ -6 dB(절반 볼륨)
-        //Bgmslider.value = 0.1->Mathf.Log10(0.1) * 20 = -20 dB(거의 무음)
+        LoadMixerVolume(); 
+
+        Bgmslider.onValueChanged.AddListener(SetBgmVolume);
+        Sfxslider.onValueChanged.AddListener(SetSfxVolume);
     }
 
-    public void SetSfxVolume()
+    private void LoadMixerVolume()
     {
-        audioMixer.SetFloat("SFX", Mathf.Log10((float)Sfxslider.value) * 20);
+        float bgmVolume, sfxVolume;
+
+        if (audioMixer.GetFloat("BGM", out bgmVolume))
+            Bgmslider.value = Mathf.Pow(10, bgmVolume / 20); // 데시벨 값을 0~1로 변환
+
+        if (audioMixer.GetFloat("SFX", out sfxVolume))
+            Sfxslider.value = Mathf.Pow(10, sfxVolume / 20);
+    }
+
+    public void SetBgmVolume(float value)
+    {
+        AudioManager.Instance.SetBGMVolume(value);
+    }
+
+    public void SetSfxVolume(float value)
+    {
+        AudioManager.Instance.SetSFXVolume(value);
     }
 }
