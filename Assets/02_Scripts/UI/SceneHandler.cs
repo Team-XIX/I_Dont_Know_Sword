3785e4898c63ddbab4ae201edf041b85a25504c0
Punksoda <zug3049@gmail.com>
2025-04-02 19:09:14 +0900

    [Feat] Application.quit
    GameOver by ApplicationQuit

--- Assets/02_Scripts/UI/SceneHandler.cs
+++ Assets/02_Scripts/UI/SceneHandler.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 public class SceneHandler : MonoBehaviour
 {
     public Button targetButton;
+    public Button quitButton;
     public AudioClip clickSound;
 
     private void Start()
     {
         if (targetButton != null)
             targetButton.onClick.AddListener(() => StartCoroutine(ChangeScene()));
+
+        if (quitButton != null)
+            quitButton.onClick.AddListener(QuitGame);
 
     }
     IEnumerator ChangeScene()
@@ -23,5 +27,13 @@ public class SceneHandler : MonoBehaviour
         }
 
         SceneManager.LoadScene("GameScene");
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }
