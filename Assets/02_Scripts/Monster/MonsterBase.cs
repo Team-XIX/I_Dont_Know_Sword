0ebe9a64e81a2095db6a9a21bf30d467f3e53831
kim minjun <k980128@naver.com>
2025-04-02 17:06:25 +0900

    [Refactor] Cleanup spaces between monsterBase script.

--- Assets/02_Scripts/Monster/MonsterBase.cs
+++ Assets/02_Scripts/Monster/MonsterBase.cs
@@ -60,12 +60,10 @@ public abstract class MonsterBase : MonoBehaviour, IDamageable
         if(Input.GetKeyDown(KeyCode.N))
             TakeDamage(2);
     }
-
     private void OnEnable()
     {
         monsterState = MonsterState.Idle;// 상태 초기화.
     }
-
     private void Awake()
     {
         if (monsterData != null)
@@ -105,7 +103,6 @@ public abstract class MonsterBase : MonoBehaviour, IDamageable
     {
         isQuitting = true;
     }
-
     protected virtual void OnDisable()
     {
         StopAllCoroutines();// 몬스터가 비활성화 되면 모든 코루틴을 멈춤.
@@ -147,7 +144,6 @@ public abstract class MonsterBase : MonoBehaviour, IDamageable
         }
     }
     protected abstract void MonsterDead();// 몬스터가 죽었을 때 실행되는 함수
-    
     public void SetTargetNull()// 플레이어가 다른 방으로 이동시 호출.
     {
         target = null;
@@ -164,9 +160,36 @@ public abstract class MonsterBase : MonoBehaviour, IDamageable
         }
         isBlinking = false;
     }
-
     public void DropItem()
     {
         Instantiate(dropItem, transform.position, Quaternion.identity);
     }
+
+    /*
+     * 1. Reaper, 근거리1개 + 원거리1개
+일반 패턴 : 근거리 사거리내에 접근시 근거리 공격,
+
+스킬 패턴 : target이 입력되고 처음 move state에 들어온 순간부터 쿨타임 카운트 시작. 
+스킬 1 : 플레이어가 스킬 사거리(별도의 변수) 내에 접근하면 8초마다 플레이어의 위치로 매우 빠르게 대시.
+스킬 2 : 매 14초 마다 플레이어의 근처
+3곳에 랜덤하게 스킬 소환.
+
+
+2. Dark Mage 원거리 2개
+스킬 패턴 : target이 입력되고 처음 move state에 들어온 순간부터 쿨타임 카운트 시작.
+매 4초마다, 2초동안 플레이어와 일정 거리를 두고, 거리를 둔 이후 스킬 1, 2 둘중 하나를 랜덤으로 사용.
+스킬 1 : target이 있던 방향으로 벽에 닿으면 파괴되는 투사체 발사.
+스킬 2 : 원형으로 투사체 12개 발사.
+
+3. Agias
+이동 패턴 : 매 10초마다 1.5초간 이동
+스킬 패턴 : 나머지는 7초에 한번씩 스킬 2 또는 3을 사용(리퍼와 같은 방식으로 스킬 시전 구성) >> 스킬 1,2,3 순서대로 우선 사용.
+입장시 : 입장 후 3초 이후부터 동작.
+스킬 1(플레이어 근처 -3~3 랜덤 위치 별모양 검정 마법진 1개) : 
+매 20초마다 유도하는 작은 눈알 몬스터 각각 3마리 소환. 부딫히면 터짐 (매우 빠른 속도)
+
+스킬 2(빨간색 원이 차고 다차면 발사) : 맵에 5줄기 직선으로 광선이 뻗고 이후 3초간 회전
+스킬 3(보라색 원이 차고 다차면 발사) : 맵에 무수히 많은 발사체를 원형으로 분사
+
+    */// 엘리트 몬스터 패턴
 }
