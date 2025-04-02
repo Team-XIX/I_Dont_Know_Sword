0ec7b1f34d91fdfaf26ac403ae82678fc179a240
jsw981117 <jsw981117@naver.com>
2025-04-02 14:23:56 +0900

    [Feat] update UIManager
    UIManager에 게임 승리 시 UI 처리 추가

--- Assets/02_Scripts/Managers/UIManager.cs
+++ Assets/02_Scripts/Managers/UIManager.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIManager : SingleTon<UIManager>
 {
     [Header("UI Panels")]
     public GameObject pauseMenu;
     public GameObject gameOverMenu;
+    [SerializeField] private Image gameDone;
     //public GameObject inventoryUI;
     //public GameObject statusUI;
     //public GameObject weaponChangeUI;
@@ -20,19 +22,26 @@ public class UIManager : SingleTon<UIManager>
     [Header("Buttons")]
     //public Button pauseButton;
     public Button resumeButton;
+    public Button gameDoneButton;
     //public Button statusButton;
+
+    
 
 
     private void Start()
     {
         InitializeUI();
+        gameDoneButton.onClick.AddListener(OnClickGameDoneButton);
     }
 
     private void InitializeUI() // UI 정보 초기화
     {
         pauseMenu.SetActive(false);
         gameOverMenu.SetActive(false);
-        
+        if (gameDone != null)
+        {
+            gameDone.gameObject.SetActive(false);
+        }
     }
 
     public void TogglePause()
@@ -47,5 +56,16 @@ public class UIManager : SingleTon<UIManager>
         bool isGameOver = !gameOverMenu.activeSelf;
         gameOverMenu.SetActive(isGameOver);
         Time.timeScale = isGameOver ? 0 : 1;
+    }
+
+    public void ShowGameDone()
+    {
+        gameDone.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void OnClickGameDoneButton()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
